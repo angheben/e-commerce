b0eff934efa461222c57973670014db9bc950dbb
Vitor Angheben <vitor_angheben@hotmail.com>
2023-12-30 13:18:03 -0300

    A lot done

--- item/models.py
+++ item/models.py
@@ -17,6 +17,9 @@ class Category(models.Model):
 
 
 class Item(models.Model):
+    """
+    This class will server to display the descriptions of an item of our e-commerce
+    """
     category = models.ForeignKey(Category, related_name='items', on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
@@ -26,3 +29,5 @@ class Item(models.Model):
     created_by = models.ForeignKey(User, related_name='items', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.name
