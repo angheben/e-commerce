dc2b03317a75d5d130fc9ef24a021a48340c9a65
Vitor Angheben <vitor_angheben@hotmail.com>
2024-01-08 21:35:21 -0300

    Creating the details page

--- item/models.py
+++ item/models.py
@@ -25,8 +25,16 @@ class Item(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
     price = models.FloatField()
-    image = StdImageField('Image', upload_to='media', variations={"thumb": {"width": 225, 'height': 225, 'crop': False},
-                          "large_thumb": {"width": 600, "height": 600, "crop": False}})
+    image = StdImageField('Image', upload_to='media', variations={
+                            "thumb": {"width": 225, 'height': 225, 'crop': False}})
+    large_image = StdImageField(
+        'Large Image',
+        upload_to='media',
+        variations={
+            "large_thumb": {"width": 1600, "height": 1600, "crop": False},
+            # Add more variations if needed
+        }
+    )
     is_sold = models.BooleanField(default=False)
     created_by = models.ForeignKey(User, related_name='items', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
